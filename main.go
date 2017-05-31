44a46abd7740859b9ebba79311afac9e22a652e6
Zaq? Wiedmann <zaquestion@gmail.com>
2017-05-30 21:59:45 -0700

    implemented put, handling most errors

--- main.go
+++ main.go
@@ -6,19 +6,34 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 	"time"
 )
 
 const (
 	chunkSize = 512
 )
+const (
+	undefinedErr        = iota
+	fileNotFoundErr     = iota
+	accessViolationErr  = iota
+	diskFullErr         = iota
+	illegalOperationErr = iota
+	unknownTIDErr       = iota
+	fileExistsErr       = iota
+	noSuchUserErr       = iota
+)
 
 var (
-	ackConn map[string]chan int
+	ackChans  map[string]chan int
+	ackLock   sync.RWMutex
+	dataChans map[string]chan []byte
+	dataLock  sync.RWMutex
 )
 
 func main() {
-	ackConn = make(map[string]chan int)
+	ackChans = make(map[string]chan int)
+	dataChans = make(map[string]chan []byte)
 	pc, err := net.ListenPacket("udp", "0.0.0.0:8069")
 
 	if err != nil {
@@ -27,56 +42,130 @@ func main() {
 	defer pc.Close()
 	for {
 		buf := make([]byte, 516)
-		_, addr, err := pc.ReadFrom(buf)
+		rn, addr, err := pc.ReadFrom(buf)
 		if err != nil {
 			log.Fatal(err)
 		}
-		//log.Println("from:", addr.String(), "read:", rn, "data:", string(buf))
+		//log.Println("from:", addr.String(), "network", addr.Network(), "read:", rn, "data:", string(buf))
 
 		switch buf[1] {
-		case 1: // read
-			ack(pc, addr)
+		case 1, 2: // read, write
+			ack(pc, addr, 0, buf[1]%2)
+
 			endFileName := bytes.IndexByte(buf[2:], 0) + 2
 			endMode := bytes.IndexByte(buf[endFileName+1:], 0) + endFileName
 			if mode := string(buf[endFileName+1 : endMode+1]); mode != string([]byte("octet")) {
-				//log.Println("got mode:", mode, ", only octet allowed")
-				pc.WriteTo(append([]byte{0, 5, 0, 0}, []byte("only octet mode supported")...), addr)
+				error(pc, addr, undefinedErr, "only octet mode supported")
 				continue
 			}
 
 			filename := string(buf[2:endFileName])
 
-			go func() {
-				c := make(chan int, 0)
-				ackConn[addr.String()] = c
-				sendFile(pc, addr, filename, c)
-				close(c)
-			}()
+			switch buf[1] {
+			case 1:
+				go func() {
+					c := make(chan int, 0)
+
+					ackLock.Lock()
+					ackChans[addr.String()] = c
+					ackLock.Unlock()
+
+					sendFile(pc, addr, filename, c)
+
+					ackLock.Lock()
+					delete(ackChans, addr.String())
+					ackLock.Unlock()
+
+					close(c)
+				}()
+			case 2:
+				go func() {
+					c := make(chan []byte, 0)
+
+					dataLock.Lock()
+					dataChans[addr.String()] = c
+					dataLock.Unlock()
+
+					writeFile(pc, addr, filename, c)
+
+					dataLock.Lock()
+					delete(dataChans, addr.String())
+					dataLock.Unlock()
+
+					close(c)
+				}()
+			}
+		case 3:
+			// TODO: use TID here?
+			dataLock.RLock()
+			if c, ok := dataChans[addr.String()]; ok {
+				c <- buf[2:rn]
+			}
+			dataLock.RUnlock()
 		case 4: // received ack
-			if c, ok := ackConn[addr.String()]; ok {
+			ackLock.RLock()
+			if c, ok := ackChans[addr.String()]; ok {
 				c <- int(buf[2])*256 + int(buf[3])
 			}
-		case 5:
-			// TODO: ack here? (probably)
-			ack(pc, addr)
+			ackLock.RUnlock()
+		case 5: // errors
 			endError := bytes.IndexByte(buf[4:], 0) + 4
 			log.Println("err_code:", buf[3], "err:", string(buf[4:endError]))
 		default:
-			pc.WriteTo(append([]byte{0, 5, 0, 0}, []byte("Operation Not Supported")...), addr)
+			error(pc, addr, illegalOperationErr, "illegal operation")
 		}
 	}
 }
 
-func ack(pc net.PacketConn, addr net.Addr) {
-	_, err = pc.WriteTo([]byte{0, 4, 0, 1}, addr)
+func ack(pc net.PacketConn, addr net.Addr, bh, bl byte) {
+	_, err := pc.WriteTo([]byte{0, 4, bh, bl, 0}, addr)
+	// TODO: try again instead?
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func error(pc net.PacketConn, addr net.Addr, code byte, msg string) {
+	errPacket := append([]byte{0, 5, 0, code}, []byte(msg)...)
+	errPacket = append(errPacket, 0)
+	pc.WriteTo(errPacket, addr)
+}
+
+func writeFile(pc net.PacketConn, addr net.Addr, filename string, data <-chan []byte) {
+	if _, err := os.Stat("files/" + filename); !os.IsNotExist(err) {
+		error(pc, addr, fileExistsErr, "file exists")
+		return
+	}
+	file, err := os.Create("files/" + filename)
+	if err != nil {
+		error(pc, addr, 0, err.Error())
+		return
+	}
+	defer file.Close()
+
+	for {
+		buf := <-data
+		wn, err := file.Write(buf[2:])
+		if err != nil {
+			error(pc, addr, 0, err.Error())
+			return
+		}
+		if wn < 512 {
+			err := file.Sync()
+			if err != nil {
+				error(pc, addr, 0, err.Error())
+				return
+			}
+			ack(pc, addr, buf[0], buf[1])
+			break
+		}
+		ack(pc, addr, buf[0], buf[1])
+	}
+}
+
 func sendFile(pc net.PacketConn, addr net.Addr, filename string, ack <-chan int) {
 	if _, err := os.Stat("files/" + filename); os.IsNotExist(err) {
-		pc.WriteTo([]byte{0, 5, 0, 1, 0}, addr)
+		error(pc, addr, fileNotFoundErr, "file not found")
 		return
 	}
 	file, err := os.Open("files/" + filename)
@@ -90,8 +179,9 @@ func sendFile(pc net.PacketConn, addr net.Addr, filename string, ack <-chan int)
 		if b > 65535 {
 			b = 0 // large file handling
 		}
+		// data packet header
 		buf.Write([]byte{0, 3, byte(b / 256 % 512), byte(b % 256)})
-		copied, err := io.CopyN(buf, file, 512)
+		wn, err := io.CopyN(buf, file, 512)
 		if err != nil && err != io.EOF {
 			log.Fatal(err)
 		}
@@ -115,13 +205,19 @@ func sendFile(pc net.PacketConn, addr net.Addr, filename string, ack <-chan int)
 					log.Println("Ack Block MisMatch got:", ackBlock, "expected:", b)
 				}
 			case <-time.After(time.Second * 5):
-				// no ack resend
+				// no ack received
 				sendBlock()
+				continue
+			case <-time.After(time.Second * 24):
+				// Just before 5th retry, timeout
+				log.Println("get operation timed out")
+				return
 			}
 			break
 		}
-		if copied < 512 {
-			delete(ackConn, addr.String())
+		// TODO: test filesize % 512 == 0 case
+		// works, but write real test
+		if wn < 512 {
 			break
 		}
 		buf.Reset()
